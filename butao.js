240c3f0532e88d9554ce530f5063111c400e1305
HadokBjorn <esaubandeira.dev@gmail.com>
2022-12-23 14:29:58 -0500

    update button

--- butao.js
+++ butao.js
@@ -1,12 +1,23 @@
 const btnPrev = document.getElementById("prev");
+const btnNext = document.getElementById("next");
 const firstCard = document.getElementById("firs-card");
 
+let cont = 0;
 function esquerda(){
         document.querySelector('.story').scrollBy(-100,0);
         console.log(firstCard.offsetLeft);
+        cont--;
+        if(cont<=0){
+                btnPrev.style.display = "none";
+                btnNext.style.display = "block";
+        }
 }
 
 function direita(){
         btnPrev.style.display = "block";
         document.querySelector('.story').scrollBy(100,0);
+        cont++;
+        if(cont>=3){
+                btnNext.style.display = "none";
+        }
 }
